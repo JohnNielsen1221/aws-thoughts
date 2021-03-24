0118c64d8405b84040876700efab3662190f25a7
John Nielsen <johnhayesnielsen@gmail.com>
2021-03-24 14:52:51 -0600

    made call to dunamo instance to create table

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -9,4 +9,28 @@ AWS.config.update({
 // Create DynamoDB service object
 const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' })
 
+// Create params object that will hold the schema and metadata of the table
+const params = {
+  TableName: 'Thoughts',
+  KeySchema: [
+    { AttributeName: 'username', KeyType: 'HASH' }, // Partition Key
+    { AttributeName: 'createdAt', KeyType: 'RANGE' } // Sort Key
+  ],
+  AttributeDefinitions: [
+    { AttributeName: 'username', AttributeType: 'S' }, // Assigning data types to the attributes
+    { AttributeName: 'createdAt', AttributeType: 'N' }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10, // Assigning read/write maximum capacity
+    WriteCapcityUnits: 10
+  }
+}
 
+// Make a call to the DynamoDB instance and create a table
+dynamodb.createTable(params, (err, data) => {
+  if (err) {
+    console.error('Unable to create table. Error JSON:', JSON.stringify(err, null, 2))
+  } else {
+    console.log('Created table. Table description JSON:', JSON.stringify(data, null, 2))
+  }
+})
