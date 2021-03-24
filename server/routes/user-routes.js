b1808112d578573e6b37af184aa0fe6c8aabcf46
John Nielsen <johnhayesnielsen@gmail.com>
2021-03-24 17:12:06 -0600

    get request not working

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -4,10 +4,27 @@ const AWS = require('aws-sdk')
 
 const awsConfig = {
   region: 'us-east-2',
-  enndpoint: 'http://localhost:8000',
+  enndpoint: 'http://localhost:8000'
 
 }
 
 AWS.config.update(awsConfig)
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 const table = 'Thoughts'
+
+// get users thoughts
+router.get('/users', (req, res) => {
+  const params = {
+    TableName: table
+  }
+  // Scan return all items in the table
+  dynamodb.scan(params, (err, data) => {
+    if (err) {
+      res.status(500).json(err) // if error occured
+    } else {
+      res.json(data.Items)
+    }
+  })
+})
+
+module.exports = router
