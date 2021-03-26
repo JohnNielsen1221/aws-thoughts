ab17f1762b958edbe231bc1a620773f110fb59cb
John Nielsen <johnhayesnielsen@gmail.com>
2021-03-26 16:56:43 -0600

    added ability to post new thought

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,69 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const ThoughtForm = () => {
-  const [formState, setFormState] = useState({ username: '', thought: '' });
-  const [characterCount, setCharacterCount] = useState(0);
+  const [formState, setFormState] = useState({ username: '', thought: '' })
+  const [characterCount, setCharacterCount] = useState(0)
 
   // update state based on form input changes
   const handleChange = event => {
     if (event.target.value.length <= 280) {
-      setFormState({ ...formState, [event.target.name]: event.target.value });
-      setCharacterCount(event.target.value.length);
+      setFormState({ ...formState, [event.target.name]: event.target.value })
+      setCharacterCount(event.target.value.length)
     }
-  };
+  }
 
   // submit form
   const handleFormSubmit = event => {
-    event.preventDefault();
+    event.preventDefault()
     // POST method with formState
     const postData = async () => {
-      const res = await fetch('/api/users', {
+      const res = await fetch('/api/users', { // use fetch function to send form data to endpoint in body of request
         method: 'POST',
         headers: {
-          Accept: 'application/json',
+          Accept: 'aplication/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(formState)
       })
-      const data = await res.json();
-      console.log(data);
+      const data = await res.json()
+      console.log(data)
     }
-    postData();
+    postData()
     // clear form value
-    setFormState({ username: '', thought: '' });
-    setCharacterCount(0);
-  };
+    setFormState({ username: '', thought: '' })
+    setCharacterCount(0)
+  }
 
   return (
     <div>
       <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
         Character Count: {characterCount}/280
-          {/* {error && <span className="ml-2">Something went wrong...</span>} */}
+        {/* {error && <span className='ml-2'>Something went wrong...</span>} */}
       </p>
       <form
-        className="flex-row justify-center justify-space-between-md align-stretch"
+        className='flex-row justify-center justify-space-between-md align-stretch'
         onSubmit={handleFormSubmit}
       >
         <input
-          placeholder="Name"
-          name="username"
+          placeholder='Name'
+          name='username'
           value={formState.username}
-          className="form-input col-12 col-md-9"
+          className='form-input col-12 col-md-9'
           onChange={handleChange}
-        ></input>
+        />
         <textarea
           placeholder="Here's a new thought..."
-          name="thought"
+          name='thought'
           value={formState.thought}
-          className="form-input col-12 col-md-9"
+          className='form-input col-12 col-md-9'
           onChange={handleChange}
-        ></textarea>
-        <button className="btn col-12 col-md-3" type="submit">
+        />
+        <button className='btn col-12 col-md-3' type='submit'>
           Submit
-          </button>
+        </button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default ThoughtForm;
+export default ThoughtForm
