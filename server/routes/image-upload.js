e50692ede2c11c3ed88662e347bd66821018d998
John Nielsen <johnhayesnielsen@gmail.com>
2021-03-29 14:46:18 -0600

    added form to upload image and finctionality to store image in bucket

--- server/routes/image-upload.js
+++ server/routes/image-upload.js
@@ -5,27 +5,27 @@ const AWS = require('aws-sdk')
 const paramsConfig = require('../utils/params-config')
 
 // Create a temporrary storage container to hold image until reat to be uploaded to S3 bucket
-const storage - multer.memoryStorage({
-  destination: function(req, file, callback) {
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
     callback(null, '')
   }
 })
 
 // Storage destination and key
-const upload = multer({storage}).single('image')
+const upload = multer({ storage }).single('image')
 
 // instantiate service object (s3) to communicate with S3 web serviice, allowing us to upload image to bucket
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01' // Lock version number in case default S3 version changes
 })
 
-router.post('/image-upload', upload, (req,res) => {
+router.post('/image-upload', upload, (req, res) => {
   console.log("post('/api/image-upload'", req.file)
   // params config
   const params = paramsConfig(req.file) // Retrieved image file object req.file from route using multer. Assigned returned object from paramsConfig function to params object
   // S3 service call
   s3.upload(params, (err, data) => {
-    if(err) {
+    if (err) {
       console.log(err)
       res.status(500).send(err)
     }
--- server/utils/params-config.js
+++ server/utils/params-config.js
@@ -6,7 +6,7 @@ const params = fileName => { // Received fileName from Express route
   const fileType = myFile[myFile.length - 1]
 
   const imageParams = {
-    Bucket: '<My_Bucket_Name>',
+    Bucket: 'user-images-4a88b81f-8bf2-4987-a7e1-9acb5f5e84b8', // Name of the already created bucket found in S3 console
     Key: `${uuidv4()}.${fileType}`,
     Body: fileName.buffer
   }
