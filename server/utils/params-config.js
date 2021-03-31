135b88d1b53ff3e784b908647e3bb89d8ceee714
John Nielsen <johnhayesnielsen@gmail.com>
2021-03-31 11:26:51 -0600

    enabled image viewing on thoughts

--- server/utils/params-config.js
+++ server/utils/params-config.js
@@ -8,7 +8,8 @@ const params = fileName => { // Received fileName from Express route
   const imageParams = {
     Bucket: 'user-images-4a88b81f-8bf2-4987-a7e1-9acb5f5e84b8', // Name of the already created bucket found in S3 console
     Key: `${uuidv4()}.${fileType}`,
-    Body: fileName.buffer
+    Body: fileName.buffer,
+    ACL: 'public-read' // allows read access to the file
   }
 
   return imageParams
